6cb460a090049e85c681da8233766929a6feb5b7
Alexey Borodenko <kosunix@gmail.com>
2017-12-05 22:21:51 +0200

    Update LayoutComponent.

--- src/ImageGallery.Client/wwwroot/app/layout/layout.component.ts
+++ src/ImageGallery.Client/wwwroot/app/layout/layout.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
     selector: 'app-layout',
     templateUrl: './layout.component.html',
-    styleUrls: ['./layout.component.scss']
+    styleUrls: ['./layout.component.scss'],
+    providers: [AuthenticationService]
 })
 export class LayoutComponent implements OnInit {
 
-    constructor() { }
+    constructor(private readonly authenticationService: AuthenticationService) { }
 
     ngOnInit() {
+        console.log(`[ngOnInit] app-layout`);
+    }
+
+    public onLogout() {
+        this.authenticationService.logout()
+            .subscribe((response) => { },
+            (err: any) => console.log(err),
+            () => console.log('logout()'));;
     }
 
 }
