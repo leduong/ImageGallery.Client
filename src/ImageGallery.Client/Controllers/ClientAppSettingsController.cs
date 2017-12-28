54189063ab3129ccca5f85ea9bce674dd2a508a1
Alexey Borodenko <kosunix@gmail.com>
2017-12-28 19:49:12 +0200

    Add ClientAppSettingsController.

---
+++ src/ImageGallery.Client/Controllers/ClientAppSettingsController.cs
@@ -0,0 +1,23 @@
+﻿using ImageGallery.Client.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ImageGallery.Client.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClientAppSettingsController : Controller
+    {
+        private readonly OpenIdConnectConfiguration _clientAppSettings;
+
+        public ClientAppSettingsController(IOptions<OpenIdConnectConfiguration> clientAppSettings)
+        {
+            _clientAppSettings = clientAppSettings.Value;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_clientAppSettings);
+        }
+    }
+}
