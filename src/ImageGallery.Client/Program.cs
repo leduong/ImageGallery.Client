d30bbf2db03c9af83824fdc4f0ad099fc5a85056
Stuart Shay <sshay@yahoo.com>
2018-01-28 16:30:07 -0500

    Update Logging

--- src/ImageGallery.Client/Program.cs
+++ src/ImageGallery.Client/Program.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics;
 using System.IO;
 using ImageGallery.Client.Configuration;
+using Loggly;
+using Loggly.Config;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -20,11 +22,14 @@ namespace ImageGallery.Client
 
         public static int Main(string[] args)
         {
+            ConfigureLoggly();
+
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Debug)
                 .Enrich.FromLogContext()
                 .WriteTo.RollingFile(LogConfiguration.GetLoggingPath(Configuration))
+                .WriteTo.Loggly()
                 .CreateLogger();
 
             Serilog.Debugging.SelfLog.Enable(msg =>
@@ -57,5 +62,26 @@ namespace ImageGallery.Client
                 .UseSetting("detailedErrors", "true")
                 .CaptureStartupErrors(true)
                 .Build();
+
+        private static void ConfigureLoggly()
+        {
+            var logglyToken = Environment.GetEnvironmentVariable("LOGGLY_TOKEN");
+            Console.WriteLine($"LogglyToken:{logglyToken}");
+
+            var config = LogglyConfig.Instance;
+            config.CustomerToken = "c3176aed-1b75-4315-9ee6-21cf1bd84dd8";
+            config.ApplicationName = "ImageGallery.Client";
+
+            config.Transport.EndpointHostname = "logs-01.loggly.com";
+            config.Transport.EndpointPort = 443;
+            config.Transport.LogTransport = LogTransport.Https;
+
+            var ct = new ApplicationNameTag
+            {
+                Formatter = "application-{0}",
+            };
+
+            config.TagConfig.Tags.Add(ct);
+        }
     }
 }
