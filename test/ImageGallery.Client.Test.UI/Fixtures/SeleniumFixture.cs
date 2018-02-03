e56e5d6196d1a641c282d3c350aa4fd5714ad939
Stuart Shay <sshay@yahoo.com>
2018-02-02 19:24:14 -0500

    Selenium Test Project Init

---
+++ test/ImageGallery.Client.Test.UI/Fixtures/SeleniumFixture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ImageGallery.Client.Test.UI.Fixtures
+{
+    public class SeleniumFixture : IDisposable
+    {
+        public SeleniumFixture()
+        {
+            var location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            Driver = new ChromeDriver(location);
+        }
+
+        public IWebDriver Driver { get; set; }
+
+        public void Dispose()
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+    }
+}
---
+++ test/ImageGallery.Client.Test.UI/Selenium/ImageGalleryClientTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using ImageGallery.Client.Test.UI.Fixtures;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ImageGallery.Client.Test.UI.Selenium
+{
+    public class ImageGalleryClientTest : IClassFixture<SeleniumFixture>
+    {
+        private readonly IWebDriver _driver;
+
+        private readonly ITestOutputHelper _output;
+
+        public ImageGalleryClientTest(SeleniumFixture fixture, ITestOutputHelper output)
+        {
+            _driver = fixture.Driver;
+            _output = output;
+        }
+
+        [Fact]
+        [Trait("Category", "Intergration")]
+        public void ShouldLoadApplicationPage_SmokeTest()
+        {
+            _driver.Navigate().GoToUrl("https://www.google.com/webhp?ie=utf-8&oe=utf-8");
+
+            Screenshot ss = ((ITakesScreenshot)_driver).GetScreenshot();
+
+            ss.SaveAsFile($"D://SeleniumTestingScreenshot_{DateTime.Now.Ticks}.png", ScreenshotImageFormat.Png);
+
+            Assert.Equal("Google", _driver.Title);
+        }
+    }
+}
