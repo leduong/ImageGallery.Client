c7108a9a45d7c8045d655d6599c1bec125aa921b
Stuart Shay <sshay@yahoo.com>
2018-02-03 15:14:16 -0500

    Modify Docker Test Config

--- test/ImageGallery.Client.Test.UI/Fixtures/SeleniumFixture.cs
+++ test/ImageGallery.Client.Test.UI/Fixtures/SeleniumFixture.cs
@@ -3,7 +3,9 @@ using System.IO;
 using System.Reflection;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Support.UI;
 
 namespace ImageGallery.Client.Test.UI.Fixtures
 {
@@ -14,11 +16,26 @@ namespace ImageGallery.Client.Test.UI.Fixtures
             var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
             Console.WriteLine("Selenium-ENV:" + env);
 
-            var location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            //var location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            var driverService = PhantomJSDriverService.CreateDefaultService(AppDomain.CurrentDomain.BaseDirectory);
+            driverService.HideCommandPromptWindow = true;
+            driverService.LoadImages = false;
+
+            var options = new PhantomJSOptions();
+            options.AddAdditionalCapability("IsJavaScriptEnabled", true);
+
+            options.AddAdditionalCapability("phantomjs.page.settings.userAgent", "Mozilla / 5.0(Windows NT 6.1) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 40.0.2214.94 Safari / 537.36");
+
+            Driver = new PhantomJSDriver(driverService, options);
+            Driver.Manage().Window.Size = new System.Drawing.Size(1280, 1024);
+
+
+
 
             // Driver = new ChromeDriver(location);
 
-            Driver = new PhantomJSDriver(location);
+            //Driver = new PhantomJSDriver(location,);
         }
 
         public IWebDriver Driver { get; set; }
