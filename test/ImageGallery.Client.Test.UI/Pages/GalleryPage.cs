7bc51fe6f69f1ac6b9bf90d7f9ac0b5f9a8a1173
Artyom Avetisyan <artyom.avetisyan@gmail.com>
2018-02-06 23:50:52 +0100

    BasePage is implemented to encapsulate future pages basic functionality. Login with basic user and privileged user is tested. Error messages with invalid login attempt is validated.

---
+++ test/ImageGallery.Client.Test.UI/Pages/GalleryPage.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace ImageGallery.Client.Test.UI.Pages
+{
+    public class GalleryPage : BaseSecurePage
+    {
+        public GalleryPage(IWebDriver driver)
+            : base(driver)
+        {
+        }
+
+        public GalleryPage(IWebDriver driver, string url)
+            : base(driver, url)
+        {
+        }
+
+        [FindsBy(How = How.LinkText, Using = "Add an image")]
+        protected IWebElement AddImageButton { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = ".danger.validation-summary-errors")]
+        protected IWebElement ValidationErrorText { get; set; }
+
+        public bool IsAddImageButtonAvailable()
+        {
+            bool buttonIsAvailable;
+            try
+            {
+                AddImageButton = LoadElement(nameof(AddImageButton));
+                var text = AddImageButton.Text;
+                buttonIsAvailable = true;
+            }
+            catch (NoSuchElementException)
+            {
+                buttonIsAvailable = false;
+            }
+            catch (WebDriverTimeoutException)
+            {
+                buttonIsAvailable = false;
+            }
+
+            return buttonIsAvailable;
+        }
+
+
+        public string GetValidationErrorText()
+        {
+            ValidationErrorText = LoadElement(nameof(ValidationErrorText));
+            return ValidationErrorText.Text;
+        }
+    }
+}
--- test/ImageGallery.Client.Test.UI/Selenium/ImageGalleryClientTest.cs
+++ test/ImageGallery.Client.Test.UI/Selenium/ImageGalleryClientTest.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Reflection;
 using ImageGallery.Client.Test.UI.Fixtures;
+using ImageGallery.Client.Test.UI.Pages;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
 using Xunit;
@@ -9,16 +10,28 @@ using Xunit.Abstractions;
 
 namespace ImageGallery.Client.Test.UI.Selenium
 {
-    public class ImageGalleryClientTest : IClassFixture<ConfigFixture>, IClassFixture<SeleniumFixture>
+    public class ImageGalleryClientTest
     {
+        private const string BasicUserName = "Frank";
+        private const string BasicUserPassword = "password";
+        private const string PrivilegedUserName = "Claire";
+        private const string PrivilegedUserPassword = "password";
+        private const string IncorrectPassword = "WRONG_PASSWORD";
+        private const string LoginRequiredMessage = "The Username field is required.";
+        private const string PasswordRequiredessage = "The Password field is required.";
+        private const string InvalidLoginMessage = "Invalid username or password";
+        private const string HomePageUrl = "https://imagegallery-client.informationcart.com/";
+
         private readonly IWebDriver _driver;
 
         private readonly ITestOutputHelper _output;
 
         private readonly string _artifactsDirectory;
 
-        public ImageGalleryClientTest(ConfigFixture configFixture, SeleniumFixture fixture, ITestOutputHelper output)
+        public ImageGalleryClientTest(ITestOutputHelper output)
         {
+            ConfigFixture configFixture = new ConfigFixture();
+            SeleniumFixture fixture = new SeleniumFixture();
             _artifactsDirectory = configFixture.ArtifactsDirectory;
             _driver = fixture.Driver;
             _output = output;
@@ -36,6 +49,73 @@ namespace ImageGallery.Client.Test.UI.Selenium
             ss.SaveAsFile(filePath, ScreenshotImageFormat.Png);
 
             Assert.Equal("Google", _driver.Title);
+        }
+
+        [Fact]
+        [Trait("Category", "UI")]
+        public void BasicUserLoginTest()
+        {
+            using (var galleryPage = new GalleryPage(_driver, HomePageUrl))
+            {
+                galleryPage.Login(BasicUserName, BasicUserPassword);
+                TakeScreenshot(galleryPage);
+
+                Assert.False(
+                    galleryPage.IsAddImageButtonAvailable(),
+                    "User is logged in with elevated permissions, so 'Add Image' button should be available.");
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "UI")]
+        public void PrivilegedUserLoginTest()
+        {
+            using (var galleryPage = new GalleryPage(_driver, HomePageUrl))
+            {
+                galleryPage.Login(PrivilegedUserName, PrivilegedUserPassword);
+                TakeScreenshot(galleryPage);
+
+                Assert.True(
+                    galleryPage.IsAddImageButtonAvailable(),
+                    "User is logged in with elevated permissions, so 'Add Image' button should be available.");
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "UI")]
+        public void EmptyuUsernamePasswordTest()
+        {
+            using (var galleryPage = new GalleryPage(_driver, HomePageUrl))
+            {
+                galleryPage.Login(string.Empty, string.Empty);
+                TakeScreenshot(galleryPage);
+
+                var validationText = galleryPage.GetValidationErrorText();
+
+                Assert.Contains(LoginRequiredMessage, validationText);
+                Assert.Contains(PasswordRequiredessage, validationText);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "UI")]
+        public void IncorrectLoginAttemptTest()
+        {
+            using (var galleryPage = new GalleryPage(_driver, HomePageUrl))
+            {
+                galleryPage.Login(BasicUserName, IncorrectPassword);
+                TakeScreenshot(galleryPage);
+
+                var validationText = galleryPage.GetValidationErrorText();
+
+                Assert.Equal(InvalidLoginMessage, validationText);
+            }
+        }
+
+        private void TakeScreenshot(BasePage page)
+        {
+            var filePath = Path.Combine(_artifactsDirectory, $"Selenium_Smoke_Test_{DateTime.Now.Ticks}.png");
+            page.SaveScreenshotAs(filePath, ScreenshotImageFormat.Png);
         }
     }
 }
