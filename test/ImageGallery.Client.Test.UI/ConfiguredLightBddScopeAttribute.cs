85bf772171ef477bb868bd9f302137346fe7416c
Stuart Shay <sshay@yahoo.com>
2018-02-13 19:50:48 -0500

    Update Reports

--- test/ImageGallery.Client.Test.UI/ConfiguredLightBddScopeAttribute.cs
+++ test/ImageGallery.Client.Test.UI/ConfiguredLightBddScopeAttribute.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.IO;
 using ImageGallery.Client.Test.UI;
 using LightBDD.Core.Configuration;
 using LightBDD.Framework.Reporting.Configuration;
 using LightBDD.Framework.Reporting.Formatters;
 using LightBDD.XUnit2;
-using Navigator.Common.Helpers;
 
 [assembly: ConfiguredLightBddScope]
 namespace ImageGallery.Client.Test.UI
@@ -14,21 +12,22 @@ namespace ImageGallery.Client.Test.UI
     {
         protected override void OnConfigure(LightBddConfiguration configuration)
         {
-            string appPath = Directory.GetCurrentDirectory();
-            string reportsPath = @"../../../../../reports";
-            string reportsDirectory = Path.GetFullPath(Path.Combine(appPath, reportsPath));
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+            string reportsPath = @"~\\..\\..\\..\\reports\\";
 
-            Console.WriteLine($"ReportsDirectory:{reportsDirectory}");
-            FileHelper.CheckFilePath(reportsDirectory);
+            if (env == "Testing" || env == "Staging")
+            {
+                reportsPath = @"~\\..\\..\\..\\..\\reports\\";
+            }
 
-            var x = "{TestDateTimeUtc:yyyy-MM-dd-HH_mm_ss}_FeaturesReport.txt";
-            var y = Path.GetFullPath(Path.Combine(x, reportsDirectory));
+            Console.WriteLine($"LightBDD|ENV:{env}|ReportsPath:{reportsPath}");
 
             configuration
-                .ReportWritersConfiguration()
-                .AddFileWriter<PlainTextReportFormatter>(y);
+              .ReportWritersConfiguration()
+              .AddFileWriter<XmlReportFormatter>(reportsPath + "{TestDateTimeUtc:yyyy-MM-dd-HH_mm_ss}_FeaturesReport.xml")
+              .AddFileWriter<PlainTextReportFormatter>(reportsPath + "{TestDateTimeUtc:yyyy-MM-dd-HH_mm_ss}_FeaturesReport.txt")
+              .AddFileWriter<HtmlReportFormatter>(reportsPath + "{TestDateTimeUtc:yyyy-MM-dd-HH_mm_ss}_FeaturesReport.html");
         }
-
 
         protected override void OnSetUp()
         {
