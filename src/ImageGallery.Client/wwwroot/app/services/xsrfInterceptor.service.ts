27d05c6d68b972e706f39a2c75e0e83639d8515b
magnastrazh <magnastrazh@gmail.com>
2018-05-29 11:53:51 +0300

    XsrfInterceptor
    as suggested here https://github.com/angular/angular/issues/19885

---
+++ src/ImageGallery.Client/wwwroot/app/services/xsrfInterceptor.service.ts
@@ -0,0 +1,22 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpRequest, HttpInterceptor, HttpXsrfTokenExtractor, HttpHandler, HttpEvent } from "@angular/common/http";
+import { Observable } from 'rxjs/Observable';
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+
+@Injectable()
+export class XsrfInterceptor implements HttpInterceptor {
+  constructor(private tokenExtractor: HttpXsrfTokenExtractor) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let requestToForward = req;
+    let token = this.tokenExtractor.getToken() as string;
+    if (token !== null) {
+      requestToForward = req.clone({ setHeaders: { "X-XSRF-TOKEN": token } });
+    }
+    return next.handle(requestToForward);
+  }
+}
+
+export let HttpXSRFInterceptorProvider =
+  { provide: HTTP_INTERCEPTORS, useClass: XsrfInterceptor, multi: true };
